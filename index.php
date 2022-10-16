9bb9a81ac92174aa0a70b0012221885da9afc762
Esteban Castillo <ec2818913@gmail.com>
2022-10-15 18:46:21 -0600

    Crecion de proyecto

---
+++ index.php
@@ -0,0 +1,56 @@
+
+
+<!DOCTYPE html>
+<html >
+  <head>
+    <meta charset="UTF-8">
+    <title>Sistema de Inventario FARMACIA CLUB DE LA SALUD </title>
+	
+    
+        <link rel="stylesheet" href="assets/css/style.css">
+
+    
+  </head>
+
+  <body style="background: #47A9AF;">
+  <br/>
+  <br/>    
+    
+  
+
+    <div class="container">
+
+  <div id="login-form">
+    <img src="logo_factura.jpg" width="350" height="100"/>
+    <h3>Iniciar sesión
+</h3>
+
+    <fieldset>
+
+      <form id="form-login">
+        <input type="text" autofocus id="un" placeholder="Usuario" required autocomplete="off"> 
+
+        <input type="password" id="up" placeholder="Contraseña" required autocomplete="off">  
+
+        <input type="submit" name="log" value="Ingresar">
+      </form>
+
+    </fieldset>
+
+  </div> <!-- end login-form -->
+
+</div>
+    
+    
+    
+<script type="text/javascript" src="assets/js/jquery-1.12.3.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="assets/js/regis.js"></script>
+
+    
+  </body>
+</html>
+
+
+
+ 
